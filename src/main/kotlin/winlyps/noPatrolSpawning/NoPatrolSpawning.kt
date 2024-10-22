a13901ff6e83a2cfb988572081506ee2eb75504c
Winlypse <nts3@o2.pl>
2024-10-22 15:07:45 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noPatrolSpawning/NoPatrolSpawning.kt
@@ -0,0 +1,14 @@
+package winlyps.noPatrolSpawning
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoPatrolSpawning : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
